6388dd091176a12b269edb5cc2110ce78043a950
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2023-04-15 03:44:11 +0330

    Add User classes

---
+++ JwtWebApiSample/Models/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace JwtWebApiSample.Models
+{
+    public class UserDto
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
